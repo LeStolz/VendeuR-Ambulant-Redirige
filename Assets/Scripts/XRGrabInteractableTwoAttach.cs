306d9e8aaa69886aec2193af293530d4b04bc305
Uyen Vu <117455714+xuanuyenvu@users.noreply.github.com>
2025-12-30 02:12:57 +0100

    fixed: grab by two hand

---
+++ Assets/Scripts/XRGrabInteractableTwoAttach.cs
@@ -0,0 +1,24 @@
+using SerializableCallback;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class XRGrabInteractableTwoAttach : XRGrabInteractable
+{
+    public Transform leftAttachTransform;
+    public Transform rightAttachTransform;
+
+    protected override void OnSelectEntering(SelectEnterEventArgs args)
+    {
+        if (args.interactorObject.transform.CompareTag("LeftHand"))
+        {
+            attachTransform = leftAttachTransform;
+        }
+        else if (args.interactorObject.transform.CompareTag("RightHand"))
+        {
+            attachTransform = rightAttachTransform;
+        }
+
+        base.OnSelectEntering(args);
+    }
+}
